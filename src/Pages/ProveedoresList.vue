ab38092b46ff4e8e8e0c02e0c901d188e91f90d6
Xrivera27 <griveram27@gmail.com>
2024-09-07 23:24:05 -0600

    Fixed

--- src/Pages/ProveedoresList.vue
+++ src/Pages/ProveedoresList.vue
@@ -4,9 +4,8 @@
   </div>
   <hr>
 
-  <h1> Proveedores</h1>
-
   <div class="proveedores-wrapper">
+    <h1> Proveedores</h1>
 
     <button id="btnAdd" class="btn btn-primary" @click="openModal" style="width: 200px; white-space: nowrap;">Agregar
       Proveedor</button>
