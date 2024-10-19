657571267891fe3fc9f1d17d9e6efa9bd3f1a156
AAxxel <axelleonardo086@gmail.com>
2024-10-19 15:05:04 -0600

    Se separaron solicitudes a api de sucursales

--- services/solicitudes.js
+++ services/solicitudes.js
@@ -19,49 +19,92 @@
             console.error('Error al obtener usuario:', error); // Manejo de errores
             throw error;
         }
-    }
+    },
 
+    async fetchRegistros(parametros) {
+        try {
+            const url = `${homeUrl}${parametros}`;
+            const response = await fetch(url);
+            
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+    
+            const data = await response.json();
+            return data;
+    
+        } catch (error) {
+            console.error('Error al obtener sucursales:', error);
+            throw error; // Manejo de errores
+        }
+    },
 
+    async patchRegistro(parametros, datosActualizados){
+        try {
+            const url = `${homeUrl}${parametros}`;
+          const respuesta = await fetch(`${url}`,
+          {
+            method: 'PATCH',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datosActualizados)
+          });
+      
+          if (!respuesta.ok){
+            throw new Error(`No se pudo actualizar: ${respuesta.statusText}`);
+          }
+      
+          else return respuesta.ok;
+        } catch (error) {
+          throw new Error(`Ocurrio un error: ${error}`);
+        }
+      },
+
+      async desactivarRegistro(parametros, datosActualizados){
+        try {
+            const url = `${homeUrl}${parametros}`;
+          const respuesta = await fetch(`${url}`,
+          {
+            method: 'PATCH',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datosActualizados)
+          });
+      
+          if (!respuesta.ok){
+            throw new Error(`No se pudo actualizar: ${respuesta.statusText}`);
+          }
+      
+          else return respuesta.ok;
+        } catch (error) {
+          throw new Error(`Ocurrio un error: ${error.statusText}`);
+        }
+      },
+
+      async postRegistro(parametros, datosNuevos){
+        const url = `${homeUrl}${parametros}`;
+        try {
+          const respuesta = await fetch(`${url}`,
+          {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datosNuevos)
+          });
+      
+          if (!respuesta.ok){
+            throw new Error(`No se pudo actualizar: ${respuesta.statusText}`);
+          }
+      
+          else return respuesta.ok;
+        } catch (error) {
+          throw new Error(`Ocurrio un error: ${error.message}`);
+        }
+      }
   }
-
-// async fetchSucursal() {
-//     try {
-//       console.log(this.id_usuario);
-//         const response = await fetch(`http://localhost:3000/api/sucursales/usuario/${this.id_usuario}`);
-        
-//         if (!response.ok) {
-//             throw new Error(`Error: ${response.status}`);
-//         }
-
-//         const data = await response.json();
-//         this.sucursales = data;
-//        console.log(response);
-        
-
-//     } catch (error) {
-//         console.error('Error al obtener Sucursal:', error); // Manejo de errores
-//     }
-// },
-// async patchSucursal(index, datosActualizados){
-//   try {
-//     const respuesta = await fetch(`http://localhost:3000/api/sucursales/actualizar-sucursal/${this.sucursales[index].id_sucursal}`,
-//     {
-//       method: 'PATCH',
-//       headers: {
-//       'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(datosActualizados)
-//     });
-
-//     if (!respuesta.ok){
-//       throw new Error(`No se pudo actualizar: ${respuesta.statusText}`);
-//     }
-
-//     else return respuesta.ok;
-//   } catch (error) {
-//     throw new Error(`Ocurrio un error: ${error}`);
-//   }
-// },
 
 // async desactivarSucursal(index){
 //   const datosActualizados = {
