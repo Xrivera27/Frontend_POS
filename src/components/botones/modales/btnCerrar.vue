99060ab29c0d9e2e2be99af27b508e78db95413f
Denuar <denuarh525@gmail.com>
2024-10-23 21:20:58 -0600

    Pop ups en usuario

--- src/components/botones/modales/btnCerrar.vue
+++ src/components/botones/modales/btnCerrar.vue
@@ -1,28 +1,27 @@
 <template>
-    <button id="btnCerrar" class="btn">
-        {{ texto }}
-      </button>
-    
-    </template>
-    
-    <script>
-    
-    export default {
-        props: {
-           texto: {
-            type: String,
-            default: "Cerrar"
-        }
-        }
+  <button id="btnCerrar" class="btn">
+    {{ texto }}
+  </button>
+
+</template>
+
+<script>
+
+export default {
+  props: {
+    texto: {
+      type: String,
+      default: "Cerrar"
     }
-    </script>
-    
-    <style>
-    .btn {
-      padding: 8px 16px;
-      margin: 4px;
-      border: none;
-      cursor: pointer;
-    }
-    
-    </style>
+  }
+}
+</script>
+
+<style>
+.btn {
+  padding: 8px 16px;
+  margin: 4px;
+  border: none;
+  cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css"; // Asegúrate de importar los estilos
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+const app = createApp(App);
+
+// Configura el plugin de Toastification
+const options = {
+  position: POSITION.TOP_RIGHT, // Puedes cambiar la posición del toast
+  timeout: 3000, // Duración del toast
+  hideProgressBar: false, // Mostrar barra de progreso
+};
+
+app.use(router);
+app.use(Toast, options);
+
+app.mount("#app");
