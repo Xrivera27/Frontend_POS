a8460dd411d48c1fb6e716b88ce11db111e27a66
AAxxel <axelleonardo086@gmail.com>
2024-10-25 16:51:44 -0600

    Se creo archivo impuestos

---
+++ src/resources/impuestos.js
@@ -0,0 +1,17 @@
+const impuestos = [
+    {
+        id: 1,
+        nombre: "IMPUESTO_ISV_15"
+    },
+    {
+        id: 2,
+        nombre: "IMPUESTO_ISV_18"
+    },
+
+    {
+        id: 3,
+        nombre: "IMPUESTO_EXTENTO"
+    }
+];
+
+module.exports = { impuestos }
