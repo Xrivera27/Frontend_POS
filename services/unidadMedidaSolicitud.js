e1ac46fe80cdd1d39ccbb4375d8142dba354f7a3
AAxxel <axelleonardo086@gmail.com>
2024-11-30 19:13:55 -0600

    Cambiamos el for por un promise all para mas rapidez

--- services/unidadMedidaSolicitud.js
+++ services/unidadMedidaSolicitud.js
@@ -1,14 +1,40 @@
 import solicitudes from './solicitudes.js';
 
+// export const getUnidadMedidaEmpresas = async (id_usuario) => {
+//     try {
+//         const unidadMedida = await solicitudes.fetchRegistros(`/unidad-medida/${id_usuario}`);
+
+//         if( unidadMedida.length > 0 ){
+//             for (const  unidad  of unidadMedida){
+//               unidad.totalProductos = await getUnidadProducto(unidad.id_medida);
+
+//             }
+//         }
+
+//         return unidadMedida;
+//     } catch (error) {
+//         console.error('Error al obtener la unidad de medida:', error);
+
+//         throw new Error('No se pudo obtener la unidad de medida para el usuario especificado.');
+    
+//     }
+// }
+
 export const getUnidadMedidaEmpresas = async (id_usuario) => {
     try {
+
         const unidadMedida = await solicitudes.fetchRegistros(`/unidad-medida/${id_usuario}`);
 
         if( unidadMedida.length > 0 ){
-            for (const  unidad  of unidadMedida){
-              unidad.totalProductos = await getUnidadProducto(unidad.id_medida);
+          const unidadPromesas = unidadMedida.map(async(u) => {
+            u.totalProductos = await getUnidadProducto(u.id_medida);
+          });
 
-            }
+             await Promise.all(unidadPromesas);
+
+        }
+        else{
+            return unidadMedida;
         }
 
         return unidadMedida;
