dc85d9ca99b51bf228f7ef0a2ba4d4556d75e734
AAxxel <axelleonardo086@gmail.com>
2024-12-08 12:05:16 -0600

    incluimos cambio para manejo de errores en get api

--- config/getApiUrl.js
+++ config/getApiUrl.js
@@ -3,14 +3,16 @@ const environment = process.env.NODE_ENV;
 const getApi = () => {
     try {
         if(environment === 'development'){
-            console.log('estas aqui');
             return 'http://localhost:3000/api'
         }
         else if(environment === 'production'){
             return 'http://uc0skkosgswkwkgosowwoocs.34.16.113.40.sslip.io/api' 
         }
+        else {
+            console.log('No fue detectado el entorno de ejecución');
+        }
     } catch (error) {
-        console.log('No fue detectado el entorno correcto.');
+        console.error('Ocurrio un error: ', error);
     }
 }
 
